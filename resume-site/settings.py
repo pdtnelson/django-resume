23789686bb277861363b434ac30fe727c97063c8
Peter Nelson <pdtnelson@gmail.com>
2025-07-29 04:52:26 -0600

    Fix debug value

--- resume-site/settings.py
+++ resume-site/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 SECRET_KEY = 'django-insecure-!7atq29&pshrz4hgo6a0)#(*o21skzr5_on@dwc_2(b_(#b&zn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
