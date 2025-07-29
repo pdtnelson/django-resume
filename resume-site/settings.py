7decc17bf73969c68cedb3ebf54754a397721287
Peter Nelson <pdtnelson@gmail.com>
2025-07-28 21:46:12 -0600

    WIP - resume file uploads

--- resume-site/settings.py
+++ resume-site/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'resume.apps.ResumeConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -120,3 +121,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Media files (Uploaded files)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
--- resume/models.py
+++ resume/models.py
@@ -3,11 +3,17 @@ from django.db import models
 class Resume(models.Model):
     name = models.CharField(max_length=100)
     target_job = models.CharField(max_length=100)
-    filename = models.CharField(max_length=100)
+    filename = models.FileField(upload_to='resumes/')
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
 
 class Profile(models.Model):
     resume = models.ForeignKey(Resume, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.title} - {self.resume.name}"
